8ffd562f2870753f5d458667336537822d3812db
b11361213 <114211720+b11361213@users.noreply.github.com>
2023-09-27 21:03:06 +0800

    0927 - week 2 assignment

---
+++ config.ps1
@@ -0,0 +1,5 @@
+mkdir "C:/xampp"
+cp "C:/Administrator/Downloads/Web-Page-Design-main/xampp/*" "C:/xampp"
+
+rm -r "C:/xampp/htdocs"
+cp "C:/Administrator/Downloads/Web-Page-Design-main/xampp/htdocs" "C:/xampp/htdocs"
