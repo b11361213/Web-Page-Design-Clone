baf70566c85d2ab70805c2b4993f2300c651e77b
b11361213 <114211720+b11361213@users.noreply.github.com>
2023-10-07 20:10:45 +0800

    1007 - little revise

--- config.ps1
+++ config.ps1
@@ -1,5 +1,2 @@
-mkdir "C:/xampp"
-cp "C:/Administrator/Downloads/Web-Page-Design-main/xampp/*" "C:/xampp"
-
 rm -r "C:/xampp/htdocs"
 cp "C:/Administrator/Downloads/Web-Page-Design-main/xampp/htdocs" "C:/xampp/htdocs"
