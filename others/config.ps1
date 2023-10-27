cdc4640b5094a783346c76a8e231b868eb435c71
b11361213 <114211720+b11361213@users.noreply.github.com>
2023-10-27 17:55:01 +0800

    1027 - modify "others" folder, update config.ps1

--- others/config.ps1
+++ others/config.ps1
@@ -1,3 +1,12 @@
 mv 'C:\Users\Administrator\Downloads\xampp\' 'C:\xampp\'
 rm -r 'C:\xampp\htdocs\'
 xcopy 'C:\Users\Administrator\Downloads\Web-Page-Design-main\htdocs\*' 'C:\xampp\htdocs\' /e
+
+mkdir 'C:\xampp\apache\crt\'
+xcopy 'C:\Users\Administrator\Downloads\Web-Page-Design-main\others\cert.conf' 'C:\xampp\apache\crt\'
+xcopy 'C:\Users\Administrator\Downloads\Web-Page-Design-main\others\make-cert.bat' 'C:\xampp\apache\crt\'
+explorer 'C:\xampp\apache\crt\localhost'
+Start-Sleep -Seconds 20
+
+rm 'C:\xampp\apache\conf\extra\httpd-xampp.conf'
+xcopy 'C:\Users\Administrator\Downloads\Web-Page-Design-main\others\httpd-xampp.conf' 'C:\xampp\apache\conf\extra\'
